135b7c96c99aae0a6e45695163fcd6f2d5fd9207
Pavel <p.petrov94@mail.ru>
2023-09-11 03:29:27 +0300

    the fourth increment is completed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"net/http"
 
+	"github.com/11Petrov/urlshortener/config"
 	"github.com/11Petrov/urlshortener/internal/handlers"
 	"github.com/go-chi/chi"
 )
 
 func main() {
+	cfg := config.NewConfig()
+
 	r := chi.NewRouter()
 
 	r.Post("/", handlers.ShortenURL)
 	r.Get("/{id}", handlers.RedirectURL)
 
-	err := http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(cfg.Addr, r)
 	if err != nil {
 		panic(err)
 	}
---
+++ config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import "flag"
+
+type Config struct {
+	Addr    string
+	BaseURL string
+}
+
+func NewConfig() *Config {
+	cfg := &Config{}
+
+	flag.StringVar(&cfg.Addr, "a", "localhost:8080", "адрес запуска HTTP-сервера")
+	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "базовый адрес результирующего сокращённого URL")
+
+	flag.Parse()
+
+	return cfg
+}
