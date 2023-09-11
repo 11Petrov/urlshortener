1a903bed3850a52505e75d83975c23aa0bb4eaf9
Pavel <p.petrov94@mail.ru>
2023-09-11 03:50:35 +0300

    fixed iter4

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ type Config struct {
 func NewConfig() *Config {
 	cfg := &Config{}
 
-	flag.StringVar(&cfg.Addr, "a", "localhost:8080", "адрес запуска HTTP-сервера")
+	flag.StringVar(&cfg.Addr, "a", ":8080", "адрес запуска HTTP-сервера")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080/", "базовый адрес результирующего сокращённого URL")
 
 	flag.Parse()
