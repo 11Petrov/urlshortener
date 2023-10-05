7550cacd69f28abb6a3644c9cb020b62e6d36b6b
Pavel <87894913+11Petrov@users.noreply.github.com>
2023-10-05 12:21:59 +0300

    iter9

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -10,27 +10,31 @@ import (
 type Config struct {
 	ServerAddress string
 	BaseURL       string
+	FilePath      string
 }
 
 // parseFlags обрабатывает флаги командной строки и возвращает значения по умолчанию, если флаги не установлены
-func parseFlags() (string, string) {
+func parseFlags() (string, string, string) {
 	serverAddressFlag := flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
 	baseURLFlag := flag.String("b", "http://localhost:8080", "базовый адрес результирующего сокращённого URL")
+	filePathFlag := flag.String("f", "/tmp/short-url-db.json", "полное имя файла для сохранения данных в формате JSON")
+
 	flag.Parse()
-	return *serverAddressFlag, *baseURLFlag
+	return *serverAddressFlag, *baseURLFlag, *filePathFlag
 }
 
 // parseEnv обрабатывает переменные окружения и возвращает их значения
-func parseEnv() (string, string) {
+func parseEnv() (string, string, string) {
 	envServerAddress := os.Getenv("SERVER_ADDRESS")
 	envBaseURL := os.Getenv("BASE_URL")
-	return envServerAddress, envBaseURL
+	envFilePath := os.Getenv("FILE_STORAGE_PATH")
+	return envServerAddress, envBaseURL, envFilePath
 }
 
 // NewConfig создает новый экземпляр конфигурации приложения на основе флагов командной строки и переменных окружения
 func NewConfig() *Config {
-	serverAddressFlag, baseURLFlag := parseFlags()
-	envServerAddress, envBaseURL := parseEnv()
+	serverAddressFlag, baseURLFlag, filePathFlag := parseFlags()
+	envServerAddress, envBaseURL, envFilePath := parseEnv()
 
 	cfg := &Config{}
 
@@ -44,6 +48,12 @@ func NewConfig() *Config {
 		cfg.BaseURL = envBaseURL
 	} else {
 		cfg.BaseURL = baseURLFlag
+	}
+
+	if envFilePath != "" {
+		cfg.FilePath = envFilePath
+	} else {
+		cfg.FilePath = filePathFlag
 	}
 
 	cfg.ServerAddress = strings.TrimPrefix(cfg.ServerAddress, "http://")
--- internal/storage/urls.go
+++ internal/storage/urls.go
@@ -1,39 +1,84 @@
 package storage
 
 import (
+	"encoding/json"
 	"errors"
+	"os"
 
 	"github.com/11Petrov/urlshortener/internal/utils"
+	"github.com/google/uuid"
 )
 
 // URLStore определяет интерфейс для хранилища URL
 type URLStore interface {
-	ShortenURL(originalURL string) string
+	ShortenURL(originalURL string) (string, error)
 	RedirectURL(shortURL string) (string, error)
+}
+
+// Event представляет информацию о сокращенном URL для сохранения в файле
+type Event struct {
+	ID          uuid.UUID `json:"uuid"`
+	ShortURL    string    `json:"short_url"`
+	OriginalURL string    `json:"original_url"`
 }
 
 // RepoURL - структура, реализующая интерфейс URLStore
 type repoURL struct {
-	URLMap map[string]string
+	URLMap  map[string]string
+	file    *os.File
+	encoder *json.Encoder
 }
 
 // NewRepoURL создает новый экземпляр RepoURL
-func NewRepoURL() URLStore {
-	return &repoURL{
-		URLMap: make(map[string]string),
+func NewRepoURL(filename string) (URLStore, error) {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
 	}
+
+	decoder := json.NewDecoder(file)
+	URLMap := make(map[string]string)
+	for {
+		var event Event
+		if err := decoder.Decode(&event); err != nil {
+			break
+		}
+		URLMap[event.ShortURL] = event.OriginalURL
+	}
+
+	return &repoURL{
+		URLMap:  URLMap,
+		file:    file,
+		encoder: json.NewEncoder(file),
+	}, err
 }
 
 // ShortenURL сокращает оригинальный URL и сохраняет его в хранилище, возвращая сокращенный URL
-func (s *repoURL) ShortenURL(originalURL string) string {
+func (r *repoURL) ShortenURL(originalURL string) (string, error) {
 	shortURL := utils.GenerateShortURL(originalURL)
-	s.URLMap[shortURL] = originalURL
-	return shortURL
+	r.URLMap[shortURL] = originalURL
+
+	event := Event{
+		ID:          uuid.New(),
+		ShortURL:    shortURL,
+		OriginalURL: originalURL,
+	}
+	data, err := json.Marshal(&event)
+	if err != nil {
+		return "", err
+	}
+
+	_, err = r.file.Write(append(data, '\n'))
+	if err != nil {
+		return "", err
+	}
+	r.file.Sync()
+	return shortURL, nil
 }
 
 // RedirectURL возвращает оригинальный URL
-func (s *repoURL) RedirectURL(shortURL string) (string, error) {
-	url, ok := s.URLMap[shortURL]
+func (r *repoURL) RedirectURL(shortURL string) (string, error) {
+	url, ok := r.URLMap[shortURL]
 	if !ok {
 		return "", errors.New("url not found")
 	}
